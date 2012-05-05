8c4c69cb1c85bd743e52fedfbedcee5bff9b7ec8
Paul <pfultz2@yahoo.com>
2012-05-04 20:27:44 -0400

    Updated RPP_DELINEATE_FROM_TO macro

--- ripple/repetition/delineate_from_to.h
+++ ripple/repetition/delineate_from_to.h
@@ -24,40 +24,32 @@
 #
 # /* RPP_DELINEATE_FROM_TO */
 #
-# if CONFIG_RIPPLE_STD
-#    define RPP_DELINEATE_FROM_TO(from, to, sep, ...) RPP_DELINEATE_FROM_TO_S(RPP_STATE(), from, to, sep, __VA_ARGS__)
-# else
-#    define RPP_DELINEATE_FROM_TO(from, to, sep, macro, data) RPP_DELINEATE_FROM_TO_S(RPP_STATE(), from, to, sep, macro, data)
-# endif
+# define RPP_DELINEATE_FROM_TO(from, to, sep, macro) RPP_DELINEATE_FROM_TO_S(RPP_STATE(), from, to, sep, macro)
 #
 # define RPP_DELINEATE_FROM_TO_ID() RPP_DELINEATE_FROM_TO
 #
 # /* RPP_DELINEATE_FROM_TO_S */
 #
-# if CONFIG_RIPPLE_STD
-#    define RPP_DELINEATE_FROM_TO_S(s, from, to, sep, ...) \
-        DETAIL_RPP_DELINEATE_FROM_TO_U(s, from, to, sep, RPP_NON_OPTIONAL(__VA_ARGS__), RPP_PACK_OPTIONAL(__VA_ARGS__)) \
+# define RPP_DELINEATE_FROM_TO_S(s, from, to, sep, macro) \
+        DETAIL_RPP_DELINEATE_FROM_TO_U(s, from, to, sep, macro) \
         /**/
-# else
-#    define RPP_DELINEATE_FROM_TO_S(s, from, to, sep, macro, data) DETAIL_RPP_DELINEATE_FROM_TO_U(s, from, to, sep, macro, (data))
-# endif
 #
 # define RPP_DELINEATE_FROM_TO_S_ID() RPP_DELINEATE_FROM_TO_S
 #
-# define DETAIL_RPP_DELINEATE_FROM_TO_U(s, from, to, sep, macro, pd) \
-    RPP_EXPR_S(s)(DETAIL_RPP_DELINEATE_FROM_TO_I(RPP_NEXT(s), RPP_NEXT(s), from, to, RPP_EMPTY, sep, macro, RPP_TRAMPOLINE(macro), pd)) \
+# define DETAIL_RPP_DELINEATE_FROM_TO_U(s, from, to, sep, macro) \
+    RPP_EXPR_S(s)(DETAIL_RPP_DELINEATE_FROM_TO_I(RPP_NEXT(s), RPP_NEXT(s), from, to, RPP_EMPTY, sep, macro, RPP_INVOKER(macro))) \
     /**/
 # define DETAIL_RPP_DELINEATE_FROM_TO_INDIRECT() DETAIL_RPP_DELINEATE_FROM_TO_I
-# define DETAIL_RPP_DELINEATE_FROM_TO_I(s, o, from, to, s1, s2, macro, _m, pd) \
+# define DETAIL_RPP_DELINEATE_FROM_TO_I(s, o, from, to, s1, s2, macro, _m) \
     RPP_IIF(RPP_NOT_EQUAL(from, to))( \
-        DETAIL_RPP_DELINEATE_FROM_TO_II, RPP_TUPLE_EAT(9) \
-    )(s, o, from, RPP_DEC(to), s1, s2, macro, _m, pd) \
+        DETAIL_RPP_DELINEATE_FROM_TO_II, RPP_EAT \
+    )(s, o, from, RPP_DEC(to), s1, s2, macro, _m) \
     /**/
-# define DETAIL_RPP_DELINEATE_FROM_TO_II(s, o, from, to, s1, s2, macro, _m, pd) \
+# define DETAIL_RPP_DELINEATE_FROM_TO_II(s, o, from, to, s1, s2, macro, _m) \
     RPP_DEFER(RPP_EXPR_S(s))(RPP_DEFER(DETAIL_RPP_DELINEATE_FROM_TO_INDIRECT)()( \
-        RPP_NEXT(s), o, from, to, s2, s2, macro, _m, pd \
+        RPP_NEXT(s), o, from, to, s2, s2, macro, _m \
     )) \
-    _m()(o, macro, to RPP_EXPOSE(pd)) s1()
+    RPP_DEFER(_m)(macro, o, to) s1()
     /**/
 #
 # endif
